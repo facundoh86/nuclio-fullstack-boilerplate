371701db53a0f43c349f1a21b9d66bd49fcf8c90
facundoherrera <facundoh1986@gmail.com>
2020-09-05 08:36:39 +0200

    "limpieza"

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-
 import './App.css';
-import UserForm from "./Component/boardform/BoardForm.view"
-import BoardForm from "./Component/boardform/BoardForm.view";
-import PinForm from "./Component/PinForm/PinForm.view";
-import LoginForm from "./Component/loginform/LoginForm.view";
+
 
 
 function App () {
   return (
-    <div className="App">
-
-        <BoardForm />
-    </div>
+    <button>Aprienta AQUI</button>
   );
 }
 
